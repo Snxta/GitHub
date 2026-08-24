366fa31e6e172e0f2f743431458243932aee4bf9
Snxta <sayantandhuya@gmail.com>
2026-08-24 19:09:15 +0530

    first commit

---
+++ Practice 2.py
@@ -0,0 +1,6 @@
+name=input ("Enter Your Name")
+if len(name) <3:
+    print("Name must be atleast 3 character long")
+elif len(name) >50:
+    print("Name must be makimus of 50 Character")
+else:("Name looks good!")
